2fc47e025988c31614517b395e05f2e9840b8e0c
jonathanjosefriasmartinez11 <jonathanjosefriasmartinez11@gmail.com>
2024-09-04 23:50:46 +0000

    Version 1

--- lib/Pages/checkout_page.dart
+++ lib/Pages/checkout_page.dart
@@ -118,6 +118,7 @@ class CheckoutPage extends StatelessWidget {
        final blob = html.Blob([pdfData]);
        final url = html.Url.createObjectUrlFromBlob(blob);
        
+       // ignore: unused_local_variable
        final anchor = html.AnchorElement(href: url)
          ..setAttribute('download', 'factura.pdf')
          ..click();
